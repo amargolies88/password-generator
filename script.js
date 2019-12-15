c0e4b7452148990e7f23137b8ed016c372dcfa52
Alan Margolies <amargolies88@gmail.com>
2019-12-15 17:11:46 -0500

    created password check that ensures required characters

--- script.js
+++ script.js
@@ -18,59 +18,119 @@ var characterList = {
     upperCase: Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
     numeric: Array.from("0123456789"),
     specialCase: Array.from(` !"#$%&'()*+,-./:;<=>?@[\\]^_\`{|}~`)
+};
+
+passwordLengthSlider.oninput = function () {
+    passwordLengthTextArea.textContent = this.value;
+};
+
+
+
+function genRandInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+var generatePassword = function () {
+    let charOptions = [];
+    let newPassword = "";
+    let newChar = "";
 
-passwordLengthSlider.oninput = function() {
-    passwordLengthTextArea.innerHTML = this.value;
-}
+    let containsLowercase = false;
+    let containsUppercase = false;
+    let containsNumeric = false;
+    let containsSpecial = false;
 
-// console.log(characterList.specialCase);
-// console.log(Array.from(characterList.lowerCase));
-// console.log(Array.from(characterList.specialCase));
-// console.log(Array.from(characterList.upperCase));
-
-
-
-function genRandInt(min, max){
-    return Math.floor( Math.random() * ( ( max - min ) + 1 ) ) + min
-}
-
-function generatePassword(){
-    var charOptions = [];
-    var newPassword = "";
-    var newChar = "";
-
-    if (!includeLowercase.checked && 
-        !includeUppercase.checked &&
-        !includeNumeric.checked &&
-        !includeSpecial.checked){
-            alert("At least 1 checkbox must be selected");
-        } else {
-            if (includeLowercase.checked){
-                charOptions = charOptions.concat(characterList.lowerCase);
+    function checkInclusions(stringToCheck) {
+        let array = stringToCheck.split("");
+        if (includeLowercase.checked) {
+            for (let i = 0; i < array.length; i++) {
+                if (characterList.lowerCase.includes(array[i])) {
+                    containsLowercase = true;
+                }
             }
-            if (includeUppercase.checked){
-                charOptions = charOptions.concat(characterList.upperCase);
-            }
-            if (includeNumeric.checked){
-                charOptions = charOptions.concat(characterList.numeric);
-            }
-            if (includeSpecial.checked){
-                charOptions = charOptions.concat(characterList.specialCase);
-            }
-
-            for (let i = 0; i < passwordLengthSlider.value; i++){
-                newChar = charOptions[genRandInt(0,charOptions.length - 1)];
-                console.log(newChar);
-                newPassword = newPassword + newChar;
+            if (!containsLowercase) {
+                return false;
             }
         }
-    passwordTextArea.textContent = newPassword;
+
+        if (includeUppercase.checked) {
+            for (let i = 0; i < array.length; i++) {
+                if (characterList.upperCase.includes(array[i])) {
+                    containsUppercase = true;
+                }
+            }
+            if (!containsUppercase) {
+                return false;
+            }
+        }
+
+        if (includeNumeric.checked) {
+            for (let i = 0; i < array.length; i++) {
+                if (characterList.numeric.includes(array[i])) {
+                    containsNumeric = true;
+                }
+            }
+            if (!containsNumeric) {
+                return false;
+            }
+        }
+
+        if (includeSpecial.checked) {
+            for (let i = 0; i < array.length; i++) {
+                if (characterList.specialCase.includes(array[i])) {
+                    containsSpecial = true;
+                }
+            }
+            if (!containsSpecial) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    if (
+        !includeLowercase.checked &&
+        !includeUppercase.checked &&
+        !includeNumeric.checked &&
+        !includeSpecial.checked
+    ) {
+        alert("At least 1 checkbox must be selected");
+    } else {
+        if (includeLowercase.checked) {
+            charOptions = charOptions.concat(characterList.lowerCase);
+        }
+        if (includeUppercase.checked) {
+            charOptions = charOptions.concat(characterList.upperCase);
+        }
+        if (includeNumeric.checked) {
+            charOptions = charOptions.concat(characterList.numeric);
+        }
+        if (includeSpecial.checked) {
+            charOptions = charOptions.concat(characterList.specialCase);
+        }
+
+        for (let i = 0; i < passwordLengthSlider.value; i++) {
+            newChar = charOptions[genRandInt(0, charOptions.length - 1)];
+            newPassword = newPassword + newChar;
+        }
+        if (checkInclusions(newPassword)){
+            console.log(`The new generated password ${newPassword} contains all required characters`)
+            passwordTextArea.textContent = newPassword;
+        } else {
+            console.log(`Password ${newPassword} is missing a required character, re-making...`);
+            generatePassword();
+        }
+    }
 }
 
-function copyPassword(){
+function copyPassword() {
     passwordTextArea.select();
     passwordTextArea.setSelectionRange(0, 99999); // For mobile devices
     document.execCommand("copy");
 }
+
+
+//ENABLES POPPER TOOLTIPS
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
